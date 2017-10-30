ca64d07bca5595d85254cd4e34839cbd8823932a
Hector <norzagaray@me.com>
2017-10-30 15:56:15 -0700

    added random generator

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var images = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'god-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imagesArray = [];
+
+//this will be a constructor function
+function Images(name) {
+  this.name = name;
+  this.path = 'assets/' + name + '.jpg';
+  this.generated = 0;
+  this.selected = 0;
+}
+//this is the constructor for new images
+for (var i = 0; i < images.length; i++){
+  imagesArray.push(new Images(images[i]));
+}
+console.log(imagesArray);
+//this is for the random generator. Will choose different number each time
+function randGen(){
+var rndG =  Math.floor(Math.random() * imagesArray.length);
+console.log('random #', rndG);
+}
+
+//randGen();
