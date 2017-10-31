65942912153d7b154768504f93131e0033b40569
Hector <norzagaray@me.com>
2017-10-31 15:07:49 -0700

    now chart displays

--- app.js
+++ app.js
@@ -159,3 +159,38 @@ image3.addEventListener('click', function() {
   imageGenerated1 = newRandom();
   document.getElementById('image1').src = 'assets/' + images[imageGenerated1] + '.jpg';
 });
+
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['banana', 'smoking', 'cars'],
+    datasets: [{
+      label: 'top 3 choices',
+      data: [19, 12, 1],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+
+      ],
+      borderWidth: 2
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
